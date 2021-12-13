4cf5e640eb415b37329734115346629cade32ad5
Antoine CLAIN <antoine.clain@hotmail.fr>
2021-12-13 15:16:29 +0100

    TP4 commit

---
+++ src/utils/ConversionNombre.java
@@ -0,0 +1,24 @@
+package utils;
+
+public class ConversionNombre {
+	
+	
+	public static void main(String[] args) {
+		
+		String chaine = "12";
+		int toto;
+		int a = 42;
+		int b = 58;
+		int nombreMax;
+		
+		toto = Integer.parseInt(chaine);
+		System.out.println(toto);
+		
+		nombreMax = Integer.max(a, b);
+		System.out.println(nombreMax);
+
+	}
+
+
+
+}
